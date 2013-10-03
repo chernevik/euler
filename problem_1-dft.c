e73f27d3c1c1d36bf97934c4c4cfad912ced89c2
Gordon Agress <gordon@practicalhorseshoeing.com>
2013-10-03 17:31:17 -0400

    further tightening

--- problem_1-dft.c
+++ problem_1-dft.c
@@ -243,3 +243,30 @@ main()
     printf("%d\n", sum_divisibles() );
 }
 
+#include <stdio.h>
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+ */
+
+/*  Answer: 233168 */
+
+int sum_divisibles()
+{
+    int ndx, sum = 0;  int LIMIT = 1000; int mult_3, mult_5;
+    while ( ++ndx ) {
+        sum += ( ( mult_3 = ndx * 3 ) < LIMIT ) * mult_3 ;
+        if ( mult_3 >= LIMIT ) { break; };
+        sum += ( ( mult_5 = ndx * 5 ) < LIMIT && mult_5 % 3 != 0 ) * mult_5;
+    };
+    return sum;
+}
+
+main()
+{
+    printf("%d\n", sum_divisibles() );
+}
+
--- problem_1.c
+++ problem_1.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
+/* http://projecteuler.net/problem=1
 
-/*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 
- */
+Answer: 233168
 
-/*  Answer: 233168 */
+*/
+
+#include <stdio.h>
 
 int sum_divisibles()
 {
-    int ndx, sum = 0;  int LIMIT = 1000; int mult_3, mult_5;
-    while ( ++ndx ) {
-        sum += ( ( mult_3 = ndx * 3 ) < LIMIT ) * mult_3 ;
-        if ( mult_3 >= LIMIT ) { break; };
-        sum += ( ( mult_5 = ndx * 5 ) < LIMIT && mult_5 % 3 != 0 ) * mult_5;
+    int ndx, mult_3, mult_5, sum = 0,  LIMIT = 1000;
+    while ( ( mult_3 = ++ndx * 3 ) < LIMIT ) {
+        sum += mult_3 + ( ( mult_5 = ndx * 5 ) < LIMIT && mult_5 % 3 != 0 ) * mult_5;
     };
     return sum;
 }
@@ -24,4 +24,11 @@ main()
 {
     printf("%d\n", sum_divisibles() );
 }
+
+
+/*
+    loop breaking from inside a switch:
+    http://stackoverflow.com/questions/1420029/how-to-break-out-of-a-loop-from-inside-a-switch
+
+ */
 
