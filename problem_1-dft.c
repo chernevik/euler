6836f8e38a26217658233d6efdff61b62a508628
Gordon Agress <gordon@practicalhorseshoeing.com>
2013-10-03 13:41:34 -0400

    Reduces the number of duplicative multiplications in answer to #1

--- problem_1-dft.c
+++ problem_1-dft.c
@@ -204,3 +204,42 @@ main()
     printf("%d\n", sum_divisibles() );
 }
 
+#include <stdio.h>
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+ */
+
+/*  Answer: 233168 */
+
+/*
+    Complaints:
+
+    Any way to reduce the number of duplicative multiplication operations?
+
+ */
+
+int sum_divisibles()
+{
+    int ndx, sum = 0;  int LIMIT = 1000;
+    while ( ++ndx ) {
+        if ( ndx * 3 < LIMIT ) {
+            sum += ndx * 3 ;
+            if ( ndx * 5 < LIMIT && ( ndx * 5 ) % 3 != 0 ) {
+                sum += ndx * 5 ;
+            }
+        } else {
+            break;
+        }
+    };
+    return sum;
+}
+
+main()
+{
+    printf("%d\n", sum_divisibles() );
+}
+
--- problem_1.c
+++ problem_1.c
@@ -9,21 +9,14 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 /*  Answer: 233168 */
 
-/*
-    Complaints:
-
-    Any way to reduce the number of duplicative multiplication operations?
-
- */
-
 int sum_divisibles()
 {
-    int ndx, sum = 0;  int LIMIT = 1000;
+    int ndx, sum = 0;  int LIMIT = 1000; int mult_3, mult_5;
     while ( ++ndx ) {
-        if ( ndx * 3 < LIMIT ) {
-            sum += ndx * 3 ;
-            if ( ndx * 5 < LIMIT && ( ndx * 5 ) % 3 != 0 ) {
-                sum += ndx * 5 ;
+        if ( ( mult_3 = ndx * 3 ) < LIMIT ) {
+            sum += mult_3;
+            if ( ( mult_5 = ndx * 5 ) < LIMIT && mult_5 % 3 != 0 ) {
+                sum += mult_5 ;
             }
         } else {
             break;
