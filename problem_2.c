49c751398db96c61cb0375a5fdfe0349eb7eb9be
Gordon Agress <gordon@practicalhorseshoeing.com>
2013-12-23 04:31:57 -0500

    Adds functions generating Fibonacci sequence

--- problem_2.c
+++ problem_2.c
@@ -10,6 +10,43 @@ By considering the terms in the Fibonacci sequence whose values do not exceed fo
  */
 
 
+int fibonacci(int first, int second)
+/*
+    Returns the next Fibonacci term, given first and second
+*/
+{
+    int nxt;
+    nxt = first + second;
+    return nxt;
+}
+
+void fibonacci_sequence(int first, int second, int max)
+/*
+    Prints out Fibonacci sequence, stopping when term exceeds max value
+ */
+{
+    int nxt;
+
+    printf("%d\n", first);
+    printf("%d\n", second);
+
+/*
+    while ( second < max ) {
+        nxt = fibonacci(first, second);
+        printf("%d\n", nxt);
+        first = second;
+        second = nxt;
+    }
+ */
+
+    while ( ( nxt = fibonacci(first, second) ) < max ) {
+        printf("%d\n", nxt);
+        first = second;
+        second = nxt;
+    }  
+}  
+
 main()
 {
+    fibonacci_sequence(1, 2, 100);
 }
