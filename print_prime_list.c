e58d6da5094162677f07ff679912be8e2e824c75
Gordon Agress <gordon@practicalhorseshoeing.com>
2014-03-02 14:48:32 -0500

    demonstrates (again) return of an array by a function

--- print_prime_list.c
+++ print_prime_list.c
@@ -1,47 +1,23 @@
 /*
 
 PROBLEM:
-Print a list of prime numbers less than N
+Print a list returned by a function
 
 MOTIVATION:
-- Demonstration basic array usage
-- Development of prime list generation algorithm
-
-THOUGHT:
-- if I'm working toward handling of pointers to arrays, wouldn't it be better to establish the pointer handling stuff with a less involved array generation problem?
+- Demonstration basic array usage, return of an array by a function
 
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void print_list() {
-/*
-    Demonstrates initialization and retrieval of array values.
-
- */
-    int i, j;
-    int ary[10];
-
-    // initialize array values
-    for (i=0; i<10; i++) 
-    {
-        ary[i] = i * 2;
-    }
-
-    // iterate and print array values
-    for (j=0; j<10; j++) 
-    {
-        printf("%d\n", ary[j]);
-    }
-}
-
-int return_list() {
+int* return_list() {
 /*
     Demonstrates return of pointer to a list
 
  */
     int i, j;
-    int ary[10];
+    int* ary = malloc(sizeof(int) * 10);
 
     // initialize array values
     for (i=0; i<10; i++) 
@@ -49,10 +25,18 @@ int return_list() {
         ary[i] = i * 2;
     }
 
+    return ary;
     // how do i return a pointer to this array?
-}
+};
 
 void main() {
-    print_list();
+    int *p_ry;
+    int j = 0;
+    int val;
+
+    p_ry = return_list();
+    for ( j=0; j < 10; j++ ) {
+        printf("%d\n", p_ry[j]);
+    }
 }
 
