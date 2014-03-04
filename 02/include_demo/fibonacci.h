5ed47d30a9e4461ca373498557978e35be019f5e
Gordon Agress <gordon@practicalhorseshoeing.com>
2014-03-04 14:54:22 -0500

    adds simple demonstration of include with my own files

---
+++ 02/include_demo/fibonacci.h
@@ -0,0 +1,36 @@
+int fibonacci(int first, int second)
+/*
+    Returns the next Fibonacci term, given first and second
+*/
+{
+    int nxt;
+    nxt = first + second;
+    return nxt;
+}
+
+
+int sum_fibonacci_even(int first, int second, int max_term)
+/*
+    Prints out sum of even terms of Fibonacci sequence less than max_term
+ */
+{
+    int nxt, total=0;
+
+    if ( first % 2 == 0 ) {
+        total += first;
+    }
+    if ( second % 2 == 0 ) {
+        total += second;
+    }
+
+    while ( ( nxt = fibonacci(first, second) ) < max_term ) {
+        /* Check for even */
+        if ( nxt % 2 == 0 ) {
+            total += nxt;
+        }
+        first = second;
+        second = nxt;
+    }  
+
+    return total;
+}  
