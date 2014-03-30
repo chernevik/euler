47ebb023c4a4b8013695a95651cca70270f183e2
Gordon Agress <gordon@practicalhorseshoeing.com>
2014-03-30 16:52:13 -0400

    further steps . . .

--- c/03/factorize.c
+++ c/03/factorize.c
@@ -13,7 +13,7 @@ int *factorize(n)
     int *primes;
     int *dividend_factors;
     int *factors = calloc(n, sizeof(int));
-    int i=0, val, factors_ndx=0, dividend, j=0, df_val;
+    int i=0, val, factors_ndx=0, dividend=0, j=0, df_val;
 
     if (is_prime(n)) {
         factors[factors_ndx] = n;
@@ -21,22 +21,15 @@ int *factorize(n)
     }
 
     primes = list_primes(n);
-    // debug shows that at some point, values in primes are reset to lots and lots of 2s
 
-    while ( ( val = primes[i] ) != -1 ) {
+    while ( ( val = primes[i] ) != -1 && !dividend ) {
         if ( n % val == 0 ) {
             factors[factors_ndx] = val;
             factors_ndx++;
             dividend = n / val;
             if (!is_prime(dividend)) {
-                // segfault before this
                 dividend_factors = factorize(dividend);
                 while ( ( df_val = dividend_factors[j] ) != -1 ) {
-                    // gdb says we're segfaulting here
-                    // value of factors_ndx is crazy high -- 258648, why so high?
-                    // MANY more values in array primes than expected -- array 100 has value
-                    // all values in primes seem to be 2
-                    // is there some problem with list_primes?
                     factors[factors_ndx] = val;
                     factors_ndx++;
                     j++;
@@ -49,7 +42,6 @@ int *factorize(n)
         i++;
     }
     factors[factors_ndx] = -1;
-    // segmentation fault seems to happen here.  why?
     return factors;
 
 }
@@ -79,5 +71,9 @@ int main()
 /*
     STATUS
 
-    generating segmentation fault: 11 when factorizing
+    segmentation fault issue fixed (had a runaway loop)
+    now printing out prime factors of 20, but too many of them
+    - now printing out the right number of prime factors for 20, but not printing 5
+        - getting 2, 2, 2, instead of 2, 2, 5
+
  */
